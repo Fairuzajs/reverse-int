c1bbd062144bf9fece1eb7ff99f11ea57fe3f4fa
fairuzajs <yaubasarova.93@mail.ru>
2023-08-02 12:29:25 +0500

    feat: test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    num = n.toString();
+    num = num.split("").reverse().join("");
+    num = Number(parseInt(num));
+    return num;
 }
